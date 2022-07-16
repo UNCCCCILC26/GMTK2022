474ee7cfe442e444140867608135ecfd0abc0c5a
dmangames1941 <61478002+dmangames1941@users.noreply.github.com>
2022-07-16 14:06:02 -0400

    Info bar
    Added Info bar, 3 recourse areas(not functional), HP bar,  On_hit function to gambler.gd, added 3 lines of code to _physics_process function in gambler.gd as well as the baase_damage var.

--- GMTK2022Project/Gambler.gd
+++ GMTK2022Project/Gambler.gd
@@ -1,6 +1,11 @@
 extends Node2D
+
+signal base_damage(damage)
+
+
 export var speed = 150
 export var strength = 10
+var base_damage = 10
 var lowerPath = load("res://LowerCurve.tres")
 var upperPath = load("res://UpperCurve.tres")
  
@@ -11,6 +16,9 @@ func _ready():
 	add_to_group("enemies")
 
 func _physics_process(delta):
+	if unit_offset == 1.0:
+		emit_signal("base_damage", base_damage)
+		queue_free()
 	move(delta)
 
 func move(delta):
@@ -21,6 +29,11 @@ func move(delta):
 func _on_VisibilityNotifier2D_screen_exited():
 	emit_signal("ReachedVault") # Replace with function body.
 
+func on_hit(damage):
+	impact()
+	hp -= damage
+	if hp <= 0:
+		on_destroy()
 
 func _on_HitBox_area_entered(area):
 	_areaEntered(area)
