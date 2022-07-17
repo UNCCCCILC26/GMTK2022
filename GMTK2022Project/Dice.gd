766561b89f7c53b95912d56793ca7d97f28e761b
kaywayz <owenmctoen@gmail.com>
2022-07-16 20:03:51 -0400

    Dice stack even less

--- GMTK2022Project/Dice.gd
+++ GMTK2022Project/Dice.gd
@@ -7,6 +7,9 @@ var is_moveable = true
 var is_r1 = false
 var is_r2 = false
 var is_r3 = false
+var child_name
+var unq_count = 0
+var is_unique = true
 
 func _ready():
 	rest_nodes = get_tree().get_nodes_in_group("zone")
@@ -35,19 +38,30 @@ func _input(event):
 			for child in rest_nodes:
 				var distance = global_position.distance_to(child.global_position)
 				if distance < shortest_dist:
-					child.select()
-					is_r3 = false
-					is_r2 = false
-					is_r1 = false
-					match child.get_name().left(7):
-						"R1 Drop":
-							is_r1 = true
-						"R2 Drop":
-							is_r2 = true
-						"R3 Drop":
-							is_r3 = true
-					rest_point = child.global_position
-					shortest_dist = distance
+					child_name = child.get_name()
+					var dices = get_tree().get_nodes_in_group("die")
+					for i in dices:
+						if i.get_class() == "Node2D":
+							if child_name == i.child_name:
+								unq_count += 1
+								if unq_count > 1:
+									is_unique = false
+					if is_unique == true:
+						child.select()
+						is_r3 = false
+						is_r2 = false
+						is_r1 = false
+						match child.get_name().left(7):
+							"R1 Drop":
+								is_r1 = true
+							"R2 Drop":
+								is_r2 = true
+							"R3 Drop":
+								is_r3 = true
+						rest_point = child.global_position
+						shortest_dist = distance
+					is_unique = true
+					unq_count = 0
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("click") and is_moveable == true:
