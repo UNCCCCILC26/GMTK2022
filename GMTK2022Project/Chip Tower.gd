9e002537e9fb1f5889d388e5e260fff7fa14e83e
kaywayz <owenmctoen@gmail.com>
2022-07-16 21:33:50 -0400

    Started attached dice to towers

--- GMTK2022Project/Chip Tower.gd
+++ GMTK2022Project/Chip Tower.gd
@@ -8,6 +8,8 @@ var shootingVars = [shootingRange, shootingSpeed, shootingDmg]
 
 var bullet = load("res://Bullet.tscn")
 var casino
+var overTop
+var overBot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +20,12 @@ func _ready():
 func _process(delta):
 	if !(shootingSpeed == shootingVars[1]):
 		$ShootingTimer.wait_time = shootingSpeed
+	overTop = $Sprite/TopDrop.get_overlapping_areas()
+	var dices = get_tree().get_nodes_in_group("die")
+	for i in dices:
+		if i.get_class() == "Node2D":
+			if i.get_child(0).get_child(0) in overTop:
+				print("HERE")
 
 func _on_ShootingTimer_timeout():
 	var furthestInRange = 0
